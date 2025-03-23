928cf74af3cba21d48d1e4248610f6ef70bcbb6b
Norbert Batiuk <norbert@batiuk.se>
2025-03-23 02:18:01 +0100

    Lazyhis: Bump version to 0.2.0

--- Formula/lazyhis.rb
+++ Formula/lazyhis.rb
@@ -1,8 +1,8 @@
 class Lazyhis < Formula
   desc "simple terminal UI for shell history"
   homepage "https://github.com/nobbmaestro/lazyhis"
-  url "https://github.com/nobbmaestro/lazyhis.git", :tag => "0.1.0"
-  sha256 "2600c7f4bd6db56dd39a5c617a278c54eef440c7ff39f1492077fa4d49b7a1db"
+  url "https://github.com/nobbmaestro/lazyhis.git", :tag => "0.2.0"
+  sha256 "91108afac472d21925069dfc41d432053bf60fa4"
   license "MIT"
 
   depends_on "go" => :build
